b7ab249b208fbb4cdb2983d6957a8b804903cc21
LauGutierrezC <130472649+LauGutierrezC@users.noreply.github.com>
2023-05-04 09:16:13 -0500

    Update guarderiacanina.sql

--- guarderiacanina.sql
+++ guarderiacanina.sql
@@ -204,7 +204,7 @@ SELECT * FROM solicitud INNER JOIN empleado ON solicitud.id_empleado = empleado.
 
 SELECT * FROM mascota INNER JOIN propietario ON mascota.id_mascota = propietario.id_propietario WHERE propietario.nombre = 'Camilo murillo';--Las mascotas que pertenecen al propietario camilo murillo
 
-SELECT factura.total,servicio.nombre FROM factura RIGHT JOIN servicio ON factura.id_servicio = servicio.id_servicio WHERE factura.id_factura IS NULL;-- Los campos que son nulos en la factura
+SELECT * FROM factura RIGHT JOIN servicio ON factura.id_factura = servicio.id_servicio;-- nos muestra de la tabla mascota y servicios.
 
 SELECT mascota.nombre,propietario.nombre FROM mascota RIGHT JOIN propietario ON mascota.id_propietario= propietario.id_propietario WHERE mascota.id_mascota IS NULL;--Los campos que son nulos en mascota
 
